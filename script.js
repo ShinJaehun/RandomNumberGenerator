5a2c944612e0986a6eb0fe8a6a3ef0e6108b86ca
ShinJaehun <gunsmoke.shin@gmail.com>
2023-12-17 15:44:16 +0900

    implement modal interface

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ pickButton.addEventListener("click", function(){
   if(balls.length>1){
     picking()
   } else {
-    console.log(balls[0])
+    // console.log(balls[0])
 
     ballDiv.innerText=balls[0].number
     ballDiv.style.backgroundColor="#"+balls[0].color
@@ -52,11 +52,12 @@ function picking(){
       let pickedNumber = Math.floor(Math.random()*balls.length-1)+1
       ballDiv.innerText = balls[pickedNumber].number
       ballDiv.style.backgroundColor="#"+balls[pickedNumber].color
+      // ballDiv.style.textShadow="-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black"
     }, 60)
 
   }else{
     stopped=true
-    pickButton.innerText = "뽑아뽑아"
+    pickButton.innerText = "쑥쑥"
     playSoundEffect()
     clearInterval(interval)
     interval=null;
@@ -70,11 +71,11 @@ function playSoundEffect(){
 }
 
 function initBalls(){
-  let numbers=5
+  let numbers=25
   for (let i=1; i<=numbers; i++){
     balls.push(new Ball(i, Math.floor(Math.random()*16777215).toString(16)))
   }
-  // console.log(balls)
+  console.log(balls)
 }
 
 function getNumber(){
@@ -88,3 +89,53 @@ function getNumber(){
   console.log(balls)
 }
 
+// Get the modal
+const settingsModal = document.getElementById("settings");
+
+// Get the button that opens the modal
+const setupBtn = document.getElementById("setup");
+
+// Get the <span> element that closes the modal
+const closeSpan = document.getElementById("close");
+const cancelBtn = document.getElementById("cancel");
+
+// When the user clicks on the button, open the modal
+setupBtn.onclick = function() {
+  settingsModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+closeSpan.onclick = function() {
+  settingsModal.style.display = "none";
+}
+cancelBtn.onclick = function() {
+  settingsModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == settingsModal) {
+    settingsModal.style.display = "none";
+  }
+}
+
+
+const settingForm = document.getElementById("setting-form");
+
+settingForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let total = document.getElementById("total");
+  let exnumber = document.getElementById("exnumber");
+
+  if (total.value == "" || exnumber.value == "") {
+    alert("Ensure you input a value in both fields!");
+  } else {
+    // perform operation with form input
+    alert("This form has been successfully submitted!");
+    console.log(
+      `This form has a 인원 of ${total.value} and 제외할 번호 of ${exnumber.value}`
+    );
+    settingsModal.style.display = "none";
+  }
+});
