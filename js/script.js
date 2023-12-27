921981c783e3ad111699ef59c834f2e5f6cfeee6
ShinJaehun <gunsmoke.shin@gmail.com>
2023-12-27 10:13:36 +0900

    input handling

--- js/script.js
+++ js/script.js
@@ -33,14 +33,26 @@ function main() {
   settingForm.addEventListener("submit", (e) => {
     e.preventDefault();
   
-    if (usernameInput.value=="" || totalInput.value == "" || exnumbersInput.value == "") {
+    if(usernameInput.value=="" || totalInput.value == "" || exnumbersInput.value == ""){
       alert("빈 칸을 모두 채우세요!");
-    } else {
+    }else if(!totalInput.value.match("^[0-9]+$")){
+      alert("'인원'에는 숫자만 입력할 수 있습니다.");
+    }else if(!exnumbersInput.value.match("^[0-9]*(,*[0-9]*)*$")){
+      alert("'제외할 번호'에는 숫자와 쉼표만 입력할 수 있습니다.")
+    }else{
       // perform operation with form input
       alert("입력한 내용을 반영합니다!");
-      storage['username']=usernameInput.value
-      storage['total']=totalInput.value
-      storage['exnumbers']=exnumbersInput.value
+      let username=usernameInput.value.trim()
+      let total=totalInput.value.trim()
+      let exnumbers=exnumbersInput.value.split(",").map(Number).filter(x=>x>0 && x<total)
+      // let exnumbers=exnumbersInput.value.split(",").filter(y => y < total).map(x => x.trim())// trim은 없어도 되는게 위에서 걸러짐!
+
+      console.log(total)
+      console.log(exnumbers)
+
+      storage['username']=username
+      storage['total']=total
+      storage['exnumbers']=exnumbers
 
       init() 
       // event listener를 setupModal.js로 옮기고 싶은데 balls를 main() 스코프 안에서 처리해야 하기 때문에 여기 있음...
