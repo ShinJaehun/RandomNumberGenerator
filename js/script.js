61d3819b5ab7c469c973d09d29c82a81ad5c4f84
ShinJaehun <gunsmoke.shin@gmail.com>
2023-12-27 15:14:06 +0900

    감격! 드디어 내가 원한 로직대로 돌아간다!

--- js/script.js
+++ js/script.js
@@ -17,18 +17,41 @@ function main() {
   pickButton.addEventListener("click", function(){
     if(balls.length>1){
       picking(balls)
-    } else {
-      // console.log(balls[0])
+    // }else if(balls.length==1){
+    //   // console.log(balls[0])
 
-      ballDiv.innerText=balls[0].number
-      ballDiv.style.backgroundColor=balls[0].color
+    //   ballDiv.innerText=balls[0].number
+    //   ballDiv.style.backgroundColor=balls[0].color
 
-      getBall(balls)
-      pickButton.innerText = "다시 시작"
-      // console.log("pickButton n: "+n)
-      initBalls(n)
-    }
-  })
+    //   getBall(balls)
+    //   // console.log("pickButton n: "+n)
+    //   // initBalls(n)
+    //   // init()
+    // }else{
+    //   pickButton.innerText = "다시 시작"
+    //   let storage=localStorage
+    //   let n=storage['total'] ?? firstN
+    //   initBalls(n) // 첨에 볼 화면에 표시할 balls 
+
+    //   // init()
+    // }
+  }else{
+    // console.log(balls[0])
+
+    ballDiv.innerText=balls[0].number
+    ballDiv.style.backgroundColor=balls[0].color
+
+    getBall(balls)
+    // console.log("pickButton n: "+n)
+    // initBalls(n)
+    // init()
+
+    pickButton.innerText = "다시 시작"
+    let storage=localStorage
+    let n=storage['total'] ?? firstN
+    initBalls(n) // 첨에 볼 화면에 표시할 balls 
+  }
+})
 
   settingForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -47,16 +70,21 @@ function main() {
       let exnumbers=exnumbersInput.value.split(",").map(Number).filter(x=>x>0 && x<total)
       // let exnumbers=exnumbersInput.value.split(",").filter(y => y < total).map(x => x.trim())// trim은 없어도 되는게 위에서 걸러짐!
 
-      console.log(total)
-      console.log(exnumbers)
+      // console.log(total)
+      // console.log(exnumbers)
 
       storage['username']=username
       storage['total']=total
       storage['exnumbers']=exnumbers
 
-      init() 
+      initBalls(total) // initBalls(total)이렇게 처리하고 init()는 시작할 때 딱 한번만 실행되게
       // event listener를 setupModal.js로 옮기고 싶은데 balls를 main() 스코프 안에서 처리해야 하기 때문에 여기 있음...
+
+      ballDiv.innerText=total
+      ballDiv.style.backgroundColor=balls[total-1].color
+
       settingsModal.style.display = "none";
+
     }
   });
 }
@@ -65,12 +93,11 @@ function init(){
   let storage=localStorage
 
   let username=storage['username'] ?? "noname"
-  n=storage['total'] ?? firstN
+  let n=storage['total'] ?? firstN
   let exnumbers=storage['exnumbers'] ?? 0
 
   console.log("init n: "+n)
 
-  balls=[]
   initBalls(n) // 첨에 볼 화면에 표시할 balls 
   ballDiv.innerText=n
   ballDiv.style.backgroundColor=balls[n-1].color
@@ -78,10 +105,13 @@ function init(){
   
 function initBalls(numbers){
   console.log("initBalls n: "+numbers)
-  // let balls=[]
+  balls=[]
   for (let i=1; i<=numbers; i++){
     balls.push(new Ball(i))
   }
   console.log(balls)
   // return balls
+  // ballDiv.innerText=numbers
+  // ballDiv.style.backgroundColor=balls[numbers-1].color
 }
+
